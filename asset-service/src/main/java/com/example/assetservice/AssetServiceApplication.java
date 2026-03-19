c9e4d7db1bb263f2811bc0a8bf782e188f38946c
alladavletshina <atdavletshina@gmail.com>
2026-03-19 22:54:15 +0300

    fix deploy again

---
+++ asset-service/src/main/java/com/example/assetservice/AssetServiceApplication.java
@@ -0,0 +1,22 @@
+package com.example.assetservice;
+
+import com.example.assetservice.util.SSLUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableRetry
+public class AssetServiceApplication {
+
+    public static void main(String[] args) {
+
+        // Отключаем проверку SSL (только для разработки/тестирования!)
+        SSLUtil.disableSSLVerification();
+
+        SpringApplication.run(AssetServiceApplication.class, args);
+    }
+
+}
