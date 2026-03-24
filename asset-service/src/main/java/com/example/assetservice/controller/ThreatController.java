57f1b5dc2afe74163c8ee8e62aa1f535b29d8ba4
alladavletshina <atdavletshina@gmail.com>
2026-03-24 22:48:30 +0300

    add pagination parametrs

--- asset-service/src/main/java/com/example/assetservice/controller/ThreatController.java
+++ asset-service/src/main/java/com/example/assetservice/controller/ThreatController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,8 @@ public class ThreatController {
             description = "Возвращает страницу угроз. Можно фильтровать по наименованию через параметр search")
     public Page<ThreatDto> getAllThreats(
             @RequestParam(required = false) @Parameter(description = "Строка для поиска по наименованию угрозы") String search,
-            @PageableDefault(size = 20) @Parameter(description = "Параметры пагинации") Pageable pageable) {
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.ASC)
+            @Parameter(description = "Параметры пагинации и сортировки") Pageable pageable) {
         Page<Threat> page;
         if (search != null && !search.isEmpty()) {
             page = threatRepository.findByNameContainingIgnoreCase(search, pageable);
